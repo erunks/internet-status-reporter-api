0a6416b4bf6bf494c8642dca22fafae3bb1b172b
Edward Runkel <eddierunkel@gmail.com>
2022-03-16 09:12:49 -0400

    Fetch a single page of results from the Outtages table.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # internet-status-reporter-admin
 ## Technology Used
 - [Rust](https://www.rust-lang.org/)
-  - [SeaORM](https://www.sea-ql.org/SeaORM/)
+  - [SeaORM](https://docs.rs/sea-orm/latest/sea_orm/)
   - [Tide](https://docs.rs/tide/latest/tide/)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+mod entity;
+use entity::prelude::*;
+
+use sea_orm::query::*;
+
+// Import the needed modules for table creation
+use sea_orm::{Database, EntityTrait};
+// Handle errors using the `https://crates.io/crates/anyhow` crate
+use anyhow::Result;
+
+#[async_std::main]
+async fn main() -> Result<()>{
+    let env_database_url = include_str!("../.env").trim();
+    let split_url: Vec<&str> = env_database_url.split("=").collect();
+    let database_url = split_url[1];
+
+    let db = Database::connect(database_url).await?;
+
+    // let outtages = Outtages::find().all(&db).await;
+    let outtage_pages = Outtages::find().order_by_desc(OuttagesColumn::CreatedAt).paginate(&db ,10);
+    let outtages = outtage_pages.fetch_page(0).await?;
+
+    // while let Some(outtages) = outtage_pages.fetch_and_next().await? {
+    //     println!("{:?}", outtages);
+    // }
+
+    println!("{:?}", outtages);
+
+    Ok(())
 }
